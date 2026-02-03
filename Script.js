c8395d593fc4551653a154e5b64b67c58072a628
shivani <patidarshivani27@gmail.com>
2026-02-03 22:02:46 +0530

    add Eventlistener to move snake

--- Script.js
+++ Script.js
@@ -7,15 +7,14 @@ const cols = Math.floor(board.clientWidth / blockWidth);
 const rows = Math.floor(board.clientHeight / blockHeight);
 
 const blocks = []
-const snake = [{
-    x:1, y:3
-},{
-    x:1, y:4
-},{
-    x:1, y:5
-}]
+const snake = [
+    {
+        x:1,y:3
+    }
+ 
 
-let direction = 'left'
+]
+let direction = 'down'
 
 
 //to access th columns
@@ -49,22 +48,43 @@ function render(){
 
 }
 
-setInterval(()=>{
-    let head = null
+// setInterval(()=>{
+//     let head = null
+//   // direction define for snake
+//     if(direction === "left"){
+//         head = { x: snake[0].x, y: snake[0].y-1}
 
-    if(direction === "left"){
-        head = { x: snake[0].x, y: snake[0].y-1}
+//     }else if ( direction === "right"){
+//         head = { x: snake[0].x,y: snake[0].y+1}
+         
+//     }else if( direction === "down"){
+//         head = {x:snake[0].x+1,y: snake[0].y}
+//     }else if(direction === "up"){
+//         head = {x:snake[0].x-1,y:snake[0].y}
+//     }
 
-    }
-
-    snake.forEach(segment => {
-        blocks[ `${segment.x}-${segment.y}`].classList.remove("fill")
-    })
+//     snake.forEach(segment => {
+//         blocks[ `${segment.x}-${segment.y}`].classList.remove("fill")
+//     })
 
 
-    snake.unshift(head)
-    snake.pop()
+//     // snake.unshift(head)
+//     // snake.pop()
     
 
-    render()
-}, 400);
+//     render()
+// }, 400);
+
+addEventListener("keyword", (event) =>{
+    if(event.key == "ArrowUp"){
+        direction = "up"
+    }else if (event.key == "ArrowRight"){
+        direction = "right"
+
+    }else if (event.key == "ArrowLeft"){
+        direction = "left"
+
+    }else if(event.key == "ArrowDown"){
+        direction = "down"
+    }
+})
